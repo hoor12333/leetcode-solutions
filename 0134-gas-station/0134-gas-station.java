9b28ec5b41418684726eab6df9638e7690796be5
hoor12333 <hoorainfatema4@gmail.com>
2025-09-09 09:41:46 -0500

    Time: 1 ms (99.9%), Space: 56.4 MB (31.42%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0, totalCost = 0;
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+        if (totalCost > totalGas) {
+            return -1;
+        }
+        int start = 0, tank = 0;
+        for (int i = 0; i < gas.length; i++) {
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return start;
+    }
+}
