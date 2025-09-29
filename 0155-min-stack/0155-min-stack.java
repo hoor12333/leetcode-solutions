e78a12a444ee124b1c5b33a79018dabd7aff2c0a
hoor12333 <hoorainfatema4@gmail.com>
2025-09-29 09:29:43 -0500

    Time: 4 ms (98.2%), Space: 45.2 MB (37.61%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class MinStack {
+    private Stack<Integer> mainStack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        mainStack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int popped = mainStack.pop();
+        if (popped == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return mainStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
